c5e74950b6e260129c5d771c0ef42df85d91888c
fullstackmedusa <alo.maldonado96@gmail.com>
2023-08-23 15:29:32 -0700

    Details page added

--- src/Components/DetailsPage.jsx
+++ src/Components/DetailsPage.jsx
@@ -1,21 +1,21 @@
-// import React from 'react';
-// import { useParams } from 'react-router-dom';
-// import './DetailsPage.css';
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import './DetailsPage.css';
 
-// const DetailsPage = ({ cardData }) => {
-//   const { id } = useParams();
-//   const selectedCard = cardData[id];
+const DetailsPage = ({ cardData }) => {
+  const { id } = useParams();
+  const selectedCard = cardData[id];
 
-//   return (
-//     <div className="details-page">
-//       <div className="details-content">
-//         <img src={selectedCard.image} alt={selectedCard.title} />
-//         <h3>{selectedCard.title}</h3>
-//         <p>ESRB Rating: {selectedCard.esrb}</p>
-//         <p>{selectedCard.description}</p>
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div className="details-page">
+      <div className="details-content">
+        <img src={selectedCard.image} alt={selectedCard.title} />
+        <h3>{selectedCard.title}</h3>
+        <p>ESRB Rating: {selectedCard.esrb}</p>
+        <p>{selectedCard.description}</p>
+      </div>
+    </div>
+  );
+};
 
-// export default DetailsPage;
+export default DetailsPage;
